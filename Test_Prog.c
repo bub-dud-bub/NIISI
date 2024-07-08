853e9b99687566922201c1d9640d9c6f3c9b7ab5
Real Serafim <61107480+bub-dud-bub@users.noreply.github.com>
2024-07-09 01:17:00 +0500

    Update Test_Prog.c

--- Test_Prog.c
+++ Test_Prog.c
@@ -2,16 +2,29 @@
 #include <locale.h>
 #include <wchar.h>
 #include <stdlib.h>
+#include <string.h>
 #define M 6
 #define N 80
 
-void graph(){
+void graph1(){
 	FILE *gnuplot = popen("gnuplot", "w");
 	if (!gnuplot) {
 	  perror("popen");
 	  exit(EXIT_FAILURE);
 	}
-	fprintf(gnuplot, "load 'plot.gnu'\n");
+	fprintf(gnuplot, "load 'plot1.gnu'\n");
+	fprintf(stdout, "Click Ctrl+d to quit...\n");
+	fflush(gnuplot);
+	getchar();
+	pclose(gnuplot);	
+}
+void graph2(){
+	FILE *gnuplot = popen("gnuplot", "w");
+	if (!gnuplot) {
+	  perror("popen");
+	  exit(EXIT_FAILURE);
+	}
+	fprintf(gnuplot, "load 'plot2.gnu'\n");
 	fprintf(stdout, "Click Ctrl+d to quit...\n");
 	fflush(gnuplot);
 	getchar();
@@ -23,41 +36,100 @@ int main(void){
   double x;
   double y;
   double z;
-  int number;
-  double a;
-  double b;
+  int t;
+  int count = 1000;
+  double Xi;
+  double average;
   int sign;
-  rectangle_uniform(-10, 10, -10, 10, 2, &x, &y);
-  printf("%f\n", section_uniform(-10, 10, 2));
-  printf("%f\n", section_uniform(-10, 10, 2));
-  printf("%f\n", section_uniform(-10, 10, 2));
+  int stat[10];
+  int size = 10000;
   // printf("%f\n", section_uniform(-6, 234, 2));
 
   // ball_normal(56, 2, &x, &y, &z);
-   //printf("%f\t%f\t%f\n", x, y, z);
-   //FILE *fp3=NULL;
-   //fp3=fopen("gen1.txt","w");
-   //for(int i=1;i<=10000;i++){
-   //  rectangle_uniform(0, 100, 0, 100, 2, &x, &y);
-   //  fprintf(fp3,"%f\t%f\t%f\n", x, y, z);
-   //}
-
-  printf("\nInput age: ");
-  scanf("%d", &number);
-  graph();
-  if (number==1){
-  	printf("\nInput: ");
-  	scanf("%d", &number);
-  	if (number==1){
-  		printf("\nInput a: ");
-  		scanf("%lf", &a);
-  		printf("\nInput b: ");
-  		scanf("%lf", &b);
-  		printf("\nInput sign: ");
-  		scanf("%d", &sign);	
-  		printf("\nResult:%f\n", section_uniform(a, b, sign));
-  		graph();
-  	}
-  }
+   
+   
+   //1:
+   FILE *fp3=NULL;
+   fp3=fopen("gen.txt","w");
+   fprintf(fp3,"%d\n", 1);
+   for(int i=1;i<=size;i++){
+     t = section_uniform(0, 9, 0);
+     fprintf(fp3,"%d\n", t);
+     stat[t]++;
+   }
+   for (int i=0;i<=9;i++){
+   	Xi += pow(stat[i] - count, 2)/count;
+   	average += abs(stat[i] - count);
+   }
+   average /= 10;
+   printf("Xi:%f\n", Xi);
+   printf("average:%f\n", average);
+   fclose(fp3);
+   graph1();
+   
+   
+   //2:
+   memset(stat, 0, sizeof(stat));
+   double n1;
+   double n2;
+   double n3;
+   double n4;
+   double Q;
+   fp3=fopen("gen.txt","w");
+   for(int i=1;i<=size;i++){
+     t = section_normal(0, 9, 0);
+     fprintf(fp3,"%d\n", t);
+     stat[t]++;
+     n1+=t;
+   }
+   n1/=size;
+   for (int i=0;i<=9;i++){
+   	n2+=pow(i-n1,2)*stat[i];
+   	n3+=pow(i-n1,3)*stat[i];
+   	n4+=pow(i-n1,4)*stat[i];
+   }
+   Q = sqrt(n2/size);
+   printf("Ax:%f\n", (n3/size)/pow(Q,3));
+   printf("Ex:%f\n", (n4/size)/pow(Q,4)-3);
+   printf("Ex:%f\n", Q);
+   fclose(fp3);
+   graph1();
+   
+   
+   //3:
+   memset(stat, 0, sizeof(stat));
+   int X;
+   int Y;
+   int stat2[10];
+   average=0;
+   Xi=0;
+   fp3=fopen("gen.txt","w");
+   for(int i=1;i<=size;i++){
+     rectangle_uniform(0, 9, -9, 0, 0, &x, &y);
+     fprintf(fp3, "%f\t%f\n", x, y);
+     X = x;
+     Y = y;
+     stat[X]++;
+     stat2[Y+9]++;
+   }
+   for (int i=0;i<=9;i++){
+   	Xi += pow(stat[i] - count, 2)/count;
+   	average += abs(stat[i] - count);
+   }
+   average /= 10;
+   printf("Xi:%f\n", Xi);
+   printf("Average:%f\n", average);
+   average=0;
+   Xi=0;
+   for (int i=0;i<=9;i++){
+   	Xi += pow(stat[i] - count, 2)/count;
+   	average += abs(stat2[i] - count);
+   }
+   average /= 10;
+   printf("Xi:%f\n", Xi);
+   printf("Average:%f\n", average);
+   fclose(fp3);
+   graph2();
+   
   return 0;
 }
